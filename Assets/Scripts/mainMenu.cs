629d3b965806554ce55d150b541289f08e1fb29b
clément <clement.becamel@epitech.eu>
2021-02-26 13:02:14 +0100

    Ajout du menu

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
