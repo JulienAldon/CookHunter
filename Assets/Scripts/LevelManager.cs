5ff6ceb5724e8c5cc3a242ed243290457eb7976f
JulienAldon <julien.aldon@epitech.eu>
2021-02-26 17:06:58 +0100

    fix menu, fix game loop, add win screen No score yet

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,9 +6,13 @@ public class LevelManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject[] tables;
+    private bool once = true;
+    public GameObject endlevel;
+    public SoundEffectMixer sound;
 
     void Start()
     {
+        sound = GameObject.FindGameObjectWithTag("Sound").GetComponent<SoundEffectMixer>();
 
     }
 
@@ -23,11 +27,13 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (CheckService()) {
-            Debug.Log("victory");
+        if (CheckService() && once) {
+            once = false;
             // Show levelcomplete text 
             
             //call scoremanager end level -> back to menu
+            endlevel.SetActive(true);
+            sound.MakeLevelClearSound();
         } 
     }
 }
